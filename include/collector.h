c9deeb3492fc5885f24d73fedade9900a875e128
yrcho <yrchodev@gmail.com>
2026-07-15 17:07:26 +0900

    docs: 코드 정렬 변경

--- include/collector.h
+++ include/collector.h
@@ -1,12 +1,18 @@
-#include <metric.h>
+#ifndef COLLECTOR_H
+#define COLLECTOR_H
+
+#include "metric.h"
+#include <stdio.h>
 
 typedef struct Collector Collector;
 
 struct Collector {
-    const char* collector_id; // collector 디버깅 용
-    int  (*init)(Collector* self);
-    int  (*collect)(Collector* self, Metric* out, size_t max_count, size_t* out_count);
-    int  (*flush)(Collector* self);
-    void (*destroy)(Collector* self);
-    void* impl_data;   // 구현체별 내부 상태 (k8s 클라이언트 핸들 등)
+    const char *collector_id; // collector 디버깅 용
+    int (*init)(Collector *self);
+    int (*collect)(Collector *self, Metric *out, size_t max_count, size_t *out_count);
+    int (*flush)(Collector *self);
+    void (*destroy)(Collector *self);
+    void *impl_data; // 구현체별 내부 상태 (k8s 클라이언트 핸들 등)
 };
+
+#endif
--- src/metric.c
+++ src/metric.c
@@ -1,6 +1,7 @@
 #include "metric.h"
+#include <string.h>
 
-Metric metric_create_app_feature(int out_res, int out_bitrate, int in_res, int in_bitrate){
+Metric metric_create_app_feature(int out_res, int out_bitrate, int in_res, int in_bitrate) {
     Metric m;
     m.type = METRIC_TYPE_APP_FEATURE;
     m.data.app_feature.output_resolution = out_res;
@@ -10,7 +11,8 @@ Metric metric_create_app_feature(int out_res, int out_bitrate, int in_res, int i
     return m;
 }
 
-Metric metric_create_system(int node_id, double cpu, double mem, const char* gpu_model, double gpu_util){
+Metric metric_create_system(int node_id, double cpu, double mem, const char *gpu_model,
+                            double gpu_util) {
     Metric m;
     m.type = METRIC_TYPE_SYSTEM;
     m.data.system.node_id = node_id;
@@ -21,7 +23,7 @@ Metric metric_create_system(int node_id, double cpu, double mem, const char* gpu
     return m;
 }
 
-Metric metric_create_app_perf(int session_id, int pid, double avg_latency, double avg_fps){
+Metric metric_create_app_perf(int session_id, int pid, double avg_latency, double avg_fps) {
     Metric m;
     m.type = METRIC_TYPE_APP_PERF;
     m.data.app_perf.session_id = session_id;
